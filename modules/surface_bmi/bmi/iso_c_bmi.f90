ac4d41e4043966062df1403d23737d836b083fd9
Nels Frazier <nels.frazier@noaa.gov>
2021-08-23 21:21:17 +0000

    fix size of arrays in get/set value functions

--- modules/surface_bmi/bmi/iso_c_bmi.f90
+++ modules/surface_bmi/bmi/iso_c_bmi.f90
@@ -370,13 +370,14 @@ module iso_c_bmif_2_0
       integer(kind=c_int) :: bmi_status
       !use a wrapper for c interop
       type(box), pointer :: bmi_box
-
+      !for determining the number of items to get
+      integer :: item_size, num_bytes, num_items
       !extract the fortran type from handle
       call c_f_pointer(this, bmi_box)
-      !FIXME this assumes dest has 1 element...what happends when it doesnt?
-      !How to determine how big this flattened array should be apriori?
-      bmi_status = bmi_box%ptr%get_value_int(c_to_f_string(name), dest(:1))
-      
+      bmi_status = bmi_box%ptr%get_var_itemsize(c_to_f_string(name), item_size)
+      bmi_status = bmi_box%ptr%get_var_nbytes(c_to_f_string(name), num_bytes)
+      num_items = item_size / num_bytes
+      bmi_status = bmi_box%ptr%get_value_int(c_to_f_string(name), dest(:num_items))
     end function get_value_int
 
     ! Get a copy of values (flattened!) of the given real variable.
@@ -387,12 +388,15 @@ module iso_c_bmif_2_0
       integer(kind=c_int) :: bmi_status
       !use a wrapper for c interop
       type(box), pointer :: bmi_box
-
+      !for determining the number of items to get
+      integer :: item_size, num_bytes, num_items
       !extract the fortran type from handle
       call c_f_pointer(this, bmi_box)
-      !FIXME this assumes dest has 1 element...what happends when it doesnt?
-      !How to determine how big this flattened array should be apriori?
-      bmi_status = bmi_box%ptr%get_value_float(c_to_f_string(name), dest(:1))
+  
+      bmi_status = bmi_box%ptr%get_var_itemsize(c_to_f_string(name), item_size)
+      bmi_status = bmi_box%ptr%get_var_nbytes(c_to_f_string(name), num_bytes)
+      num_items = item_size / num_bytes
+      bmi_status = bmi_box%ptr%get_value_float(c_to_f_string(name), dest(:num_items))
     end function get_value_float
 
     ! Get a copy of values (flattened!) of the given double variable.
@@ -401,14 +405,17 @@ module iso_c_bmif_2_0
       character(kind=c_char, len=1), dimension(BMI_MAX_COMPONENT_NAME), intent(in) :: name
       real(kind=c_double) :: dest(*)
       integer(kind=c_int) :: bmi_status
-      !use a wrapper for c interop
+            !use a wrapper for c interop
       type(box), pointer :: bmi_box
-
+      !for determining the number of items to get
+      integer :: item_size, num_bytes, num_items
       !extract the fortran type from handle
       call c_f_pointer(this, bmi_box)
-      !FIXME this assumes dest has 1 element...what happends when it doesnt?
-      !How to determine how big this flattened array should be apriori?
-      bmi_status = bmi_box%ptr%get_value_double(c_to_f_string(name), dest(:1))
+  
+      bmi_status = bmi_box%ptr%get_var_itemsize(c_to_f_string(name), item_size)
+      bmi_status = bmi_box%ptr%get_var_nbytes(c_to_f_string(name), num_bytes)
+      num_items = item_size / num_bytes
+      bmi_status = bmi_box%ptr%get_value_double(c_to_f_string(name), dest(:num_items))
     end function get_value_double
 
     ! Set new values for an integer model variable.
@@ -419,12 +426,15 @@ module iso_c_bmif_2_0
       integer(kind=c_int) :: bmi_status
       !use a wrapper for c interop
       type(box), pointer :: bmi_box
-
+      !for determining the number of items to get
+      integer :: item_size, num_bytes, num_items
       !extract the fortran type from handle
       call c_f_pointer(this, bmi_box)
-      !FIXME this assumes src has 1 element...what happends when it doesnt?
-      !How to determine how big this flattened array should be apriori?
-      bmi_status = bmi_box%ptr%set_value_int(c_to_f_string(name), src(:1))
+  
+      bmi_status = bmi_box%ptr%get_var_itemsize(c_to_f_string(name), item_size)
+      bmi_status = bmi_box%ptr%get_var_nbytes(c_to_f_string(name), num_bytes)
+      num_items = item_size / num_bytes
+      bmi_status = bmi_box%ptr%set_value_int(c_to_f_string(name), src(:num_items))
     end function set_value_int
 
     ! Set new values for a real model variable.
@@ -435,12 +445,15 @@ module iso_c_bmif_2_0
       integer(kind=c_int) :: bmi_status
       !use a wrapper for c interop
       type(box), pointer :: bmi_box
-
+      !for determining the number of items to get
+      integer :: item_size, num_bytes, num_items
       !extract the fortran type from handle
       call c_f_pointer(this, bmi_box)
-      !FIXME this assumes dest has 1 element...what happends when it doesnt?
-      !How to determine how big this flattened array should be apriori?
-      bmi_status = bmi_box%ptr%set_value_float(c_to_f_string(name), src(:1))
+  
+      bmi_status = bmi_box%ptr%get_var_itemsize(c_to_f_string(name), item_size)
+      bmi_status = bmi_box%ptr%get_var_nbytes(c_to_f_string(name), num_bytes)
+      num_items = item_size / num_bytes
+      bmi_status = bmi_box%ptr%set_value_float(c_to_f_string(name), src(:num_items))
     end function set_value_float
 
     ! Set new values for a double model variable.
@@ -451,12 +464,15 @@ module iso_c_bmif_2_0
       integer(kind=c_int) :: bmi_status
       !use a wrapper for c interop
       type(box), pointer :: bmi_box
-
+      !for determining the number of items to get
+      integer :: item_size, num_bytes, num_items
       !extract the fortran type from handle
       call c_f_pointer(this, bmi_box)
-      !FIXME this assumes dest has 1 element...what happends when it doesnt?
-      !How to determine how big this flattened array should be apriori?
-      bmi_status = bmi_box%ptr%set_value_double(c_to_f_string(name), src(:1))
+  
+      bmi_status = bmi_box%ptr%get_var_itemsize(c_to_f_string(name), item_size)
+      bmi_status = bmi_box%ptr%get_var_nbytes(c_to_f_string(name), num_bytes)
+      num_items = item_size / num_bytes
+      bmi_status = bmi_box%ptr%set_value_double(c_to_f_string(name), src(:num_items))
     end function set_value_double
 
     ! Get number of dimensions of the computational grid.
